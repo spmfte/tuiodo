f11485f48e87302eca87a96d227a25bc0cfadaa7
Aidan Littman <35202874+spmfte@users.noreply.github.com>
2025-03-17 17:54:35 -0700

    Update SHA256 checksum for v1.1.2 release

--- tuiodo.rb
+++ tuiodo.rb
@@ -2,7 +2,7 @@ class Tuiodo < Formula
   desc "A modern terminal task manager with extensive customization"
   homepage "https://github.com/spmfte/tuiodo"
   url "https://github.com/spmfte/tuiodo/archive/refs/tags/v1.1.2.tar.gz"
-  sha256 "PLACEHOLDER" # Will be updated after the tarball is published
+  sha256 "6186659032fa47685499ad52b59f9e4ea1c9c29dc18197b761d9497d46fe3581"
   license "MIT"
   head "https://github.com/spmfte/tuiodo.git", branch: "master"
 
